c0078402c2ba7f7a6d39978c8aa09ac92850187a
Don Button <donpublic@digitalgizmo.com>
2019-03-05 16:22:30 -0500

    put news list back to class view

--- mfsc/news/urls.py
+++ mfsc/news/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.newsitem_list, name='newsitem_list'),
-    # url(r'^$', views.NewsItemListView.as_view(), name='newsitem_list'),
+    url(r'^$', views.NewsItemListView.as_view(), name='newsitem_list'),
     url(r'^archive/$', views.NewsArchiveListView.as_view(), name='news_archive_list'),
     #url(r'^(?P<slug>\S+)/$', views.ProjectDetailView.as_view(), name='project_detail'),
 ]
--- mfsc/news/views.py
+++ mfsc/news/views.py
@@ -1,25 +1,13 @@
-# from datetime import date
-# import datetime
 from django.views.generic import ListView
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import NewsItem
 
 
-def newsitem_list(request):
-	"""
-	Using def instead of class -- will make it easier to use a redirect.
-	Will need to look up the short name (doubles as anchor tag).
-	"""
-	object_list = NewsItem.objects.filter(status_num__lte=2)
-	context = {'object_list': object_list}
-	return render(request, 'news/newsitem_list.html', context)
-
-# class NewsItemListView(ListView):
-#     # model = NewsItem
-#     # queryset = NewsItem.objects.filter(posted__gt=date.today() - datetime.timedelta(days=20))
-#     queryset = NewsItem.objects.filter(status_num__lte=2)
-#     # context_object_name = 'object_list'
-#     #template_name = 'news/newsitem_list.html' 
+class NewsItemListView(ListView):
+    # model = NewsItem
+    queryset = NewsItem.objects.filter(status_num__lte=2)
+    # context_object_name = 'object_list'
+    #template_name = 'news/newsitem_list.html' 
 
 class NewsArchiveListView(ListView):
     queryset = NewsItem.objects.filter(status_num=3)
